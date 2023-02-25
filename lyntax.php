347781e8bd81ca41bb2ac0f911557c1bbe23550a
Taha Amin Ghafuri <82469904+tahaghafuri@users.noreply.github.com>
2023-02-25 14:42:34 +0330

    Update lyntax.php

--- lyntax.php
+++ lyntax.php
@@ -1,57 +1,58 @@
 <?php
 
 final class lyntax{
+    
     public static function load($type,$content=null){
-    if($type==1) {
-        $content=$content;
-    }elseif($type==2){
-        $content=file_get_contents($content.'.lyntax');
-    }else{
-        return die('Please Enter Code For Lyntax!');
-    }
+        if($type==1) {
+            $content=$content;
+        }elseif($type==2){
+            $content=file_get_contents($content.'.lyntax');
+        }else{
+            return die('Please Enter Code For Lyntax!');
+        }
 
-    // Function
-    $content=lyntax::syntax('func->','function',$content);
-    $content=lyntax::syntax('pfunc','public function',$content);
-    $content=lyntax::syntax('funcp','private function',$content);
-    $content=lyntax::syntax('*|','return',$content);
+        // Function
+        $content=lyntax::syntax('func->','function',$content);
+        $content=lyntax::syntax('pfunc','public function',$content);
+        $content=lyntax::syntax('funcp','private function',$content);
+        $content=lyntax::syntax('*|','return',$content);
 
-    // Echo
-    $content=lyntax::syntax('*=','echo ',$content);
+        // Echo
+        $content=lyntax::syntax('*=','echo ',$content);
 
-    // OOP
-    $content=lyntax::syntax('%->','$this',$content);
-    $content=lyntax::syntax('%*','public $',$content);
-    $content=lyntax::syntax('%-','private $',$content);
-    $content=lyntax::syntax('%^','protected $',$content);
+        // OOP
+        $content=lyntax::syntax('%->','$this',$content);
+        $content=lyntax::syntax('%*','public $',$content);
+        $content=lyntax::syntax('%-','private $',$content);
+        $content=lyntax::syntax('%^','protected $',$content);
 
-    // Const
-    $content=lyntax::syntax('%=','define(',$content);
-    $content=lyntax::syntax('=%',');',$content);
+        // Const
+        $content=lyntax::syntax('%=','define(',$content);
+        $content=lyntax::syntax('=%',');',$content);
 
-    // Check Const
-    $content=lyntax::syntax('%=>','defined(',$content);
-    
-    
-    // Mysqli
-    $content=lyntax::syntax('$m=>','mysqli_',$content);
-        
-    // Const
-    $content=lyntax::syntax('$ ','const ',$content);
-        
-    // For
-    $content=lyntax::syntax('for=>','for(',$content);
-    $content=lyntax::syntax('<=for',')',$content);
-        
-    // While
-    $content=lyntax::syntax('while=>','while(',$content);
-    $content=lyntax::syntax('<=while',')',$content);
-        
-    // Foreach
-    $content=lyntax::syntax('foreach=>','foreach(',$content);
-    $content=lyntax::syntax('<=foreach',')',$content);
+        // Check Const
+        $content=lyntax::syntax('%=>','defined(',$content);
 
-    return eval($content);
+
+        // Mysqli
+        $content=lyntax::syntax('$m=>','mysqli_',$content);
+
+        // Const
+        $content=lyntax::syntax('$ ','const ',$content);
+
+        // For
+        $content=lyntax::syntax('for=>','for(',$content);
+        $content=lyntax::syntax('<=for',')',$content);
+
+        // While
+        $content=lyntax::syntax('while=>','while(',$content);
+        $content=lyntax::syntax('<=while',')',$content);
+
+        // Foreach
+        $content=lyntax::syntax('foreach=>','foreach(',$content);
+        $content=lyntax::syntax('<=foreach',')',$content);
+
+        return eval($content);
     }
 
     public static function get($file){
@@ -61,6 +62,7 @@ final class lyntax{
     public static function syntax($syntax,$php,$code){
         return str_replace($syntax,$php,$code);
     }
+    
 }
 
 ?>
